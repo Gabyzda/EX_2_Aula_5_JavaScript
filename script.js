c279d2413de20eb892f0bf873e2d5846136d7f41
Gaby Zapata <gabyzda@gmail.com>
2022-07-04 18:11:27 -0300

    Feito finalmente script ex 2

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 // Aula 5 Exercicio 2
 // comparar datos obtenidos con Prompt
 
-var dado = {
-    prompt('Escriba el nombre de la primera persona');
+let name1 = prompt ("Digite o nome da primeira pessoa")
+let age1 = Number(prompt ("Digite a idade da primeira pessoa"))
+let name2 = prompt ("Digite o nome da primeira pessoa") 
+let age2 = Number(prompt ("Digite a idade da segunda pessoa"))
 
-    prompt('Escriba el nombre de la segunda persona');
+console.log ("Registros de " + name1 + " e " + name2)
 
-    console.log()
+console.log ("Idade de " + name1 + ": " + age1)
+
+console.log ("Idade de " + name2 + ": " + age2)
+
+
+if (age1 >  age2) {
+  console.log (name1 + " é mais velho/a que " + name2)
 }
-var dado = prompt('Qual é a sua Idade?')
-console.log()
 
+else if(age1 <  age2) {
+  console.log (name1 + " é mais novo/a que " + name2)
+}
+
+else if(age1 == age2) {
+  console.log (name1 + " tem a mesma idade que " + name2)
+}
+
+else {
+  console.log (" eles morreram :´( ")
+}
+// else existe, caso respodam diferente a um número
