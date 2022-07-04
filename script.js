b449f256bb3e0508a516ea8f728b882c7b434573
Gaby Zapata <gabyzda@gmail.com>
2022-07-03 21:38:57 -0300

    UN desastre de JavaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+// Aula 5 Exercicio 2
+// comparar datos obtenidos con Prompt
+
+var dado = {
+    prompt('Escriba el nombre de la primera persona');
+
+    prompt('Escriba el nombre de la segunda persona');
+
+    console.log()
+}
+var dado = prompt('Qual é a sua Idade?')
+console.log()
+
